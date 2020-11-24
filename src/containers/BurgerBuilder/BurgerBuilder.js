a3a63332964ef7288708ff31a06d3dce99e57a76
MelNesbitt12 <mel.nalband@gmail.com>
2020-11-24 16:30:52 -0500

    Added/styled buttons for updating ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,14 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+
+const ingredientPrices = {
+  lettuce: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
 // handle state of burger
 class BurgerBuilder extends Component {
   state = {
@@ -11,14 +19,92 @@ class BurgerBuilder extends Component {
         bacon: 0,
         cheese: 0,
         meat: 0
-      }
+    },
+    totalPrice: 4,
+    readyToPurchase: false
+  }
+
+  // method to update the readyToPurchase state. ingredient state is passed into this function
+  updatePurchaseState = ( ingredients ) => {
+    // turn ingredients object into an array of values
+    const sum = Object.keys(ingredients).map(igKey => {
+      // return value of given key (accessing a certain property on the ingredients object, and getting its numeric value)
+      return ingredients[igKey]
+    })
+    // use reduce to turn array into a single sum of values
+      .reduce((sum, element) => {
+        return sum + element
+      }, 0)
+    this.setState({ readyToPurchase: sum > 0 })
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount + 1
+    // update ingredients
+    const updatedIngredients = {
+      ...this.state.ingredients
     }
-  // pass ingredients state to burger
+    // access the type that needs updating in the new updatedIngredients object and set value (amount of ingredients) equal to updatedCount
+    updatedIngredients[type] = updatedCount
+
+    // updating price for individual ingredients
+    const priceAddition = ingredientPrices[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice + priceAddition
+
+    // setting the state:
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    // to be sure we are passing in the most updated state of ingredients, we must pass in updatedIngredients
+    this.updatePurchaseState(updatedIngredients)
+
+  }
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type]
+    // check if we have ingredients to remove (aka no negative values)
+    if (oldCount <= 0) {
+      return
+    }
+
+    const updatedCount = oldCount - 1
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+
+    updatedIngredients[type] = updatedCount
+
+    // updating price for individual ingredients
+    const priceDeduction = ingredientPrices[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice - priceDeduction
+
+    // setting the state:
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    this.updatePurchaseState(updatedIngredients)
+  }
+
   render() {
+    // rendering buttons disabled
+    const disabledInfo = {
+      ...this.state.ingredients
+    }
+    // loop over each key in the disabledInfo object
+    for (let key in disabledInfo) {
+      // check returns true or false; if true, disable the user's ability to remove further ingredients of that type
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+    // { lettuce: true, meat: true, ...}
+    // pass ingredients state to burger
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          purchase={this.state.readyToPurchase}/>
      </Aux>
     )
   }
