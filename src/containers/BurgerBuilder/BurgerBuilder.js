5bee6b0fba23c07f9fb00c6eddb087c23b10bdb7
MelNesbitt12 <mel.nalband@gmail.com>
2020-11-24 11:05:10 -0500

    Added Burger, BurgerIngredients & Build components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+// handle state of burger
 class BurgerBuilder extends Component {
+  state = {
+      ingredients: {
+        lettuce: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+      }
+    }
+  // pass ingredients state to burger
   render() {
     return (
       <Aux>
-        <div>Burger</div>
-        <div>Build Controls</div>
-      </Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
+     </Aux>
     )
-
   }
 }
 
