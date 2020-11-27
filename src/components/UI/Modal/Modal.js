dcb7435e86321975c2e20df8580ce6da5307ae18
MelNesbitt12 <mel.nalband@gmail.com>
2020-11-27 16:14:25 -0500

    Created MenuToggle component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.module.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
 // change modal depending on show property
-const modal = ( props ) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClose}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-)
-
-export default modal
+class Modal extends Component {
+  // making sure that we do not unnecessarily re-render OrderSummary within the wrapping Modal element
+  shouldComponentUpdate (nextProps, nextState) {
+    // return true if the show property has changed
+    return nextProps.show !== this.props.show
+  }
+  componentDidUpdate () {
+    console.log('[Modal] will update')
+  }
+  render () {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClose}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+export default Modal
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.module.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import SideMenu from '../Navigation/SideMenu/SideMenu'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideMenu from '../../components/Navigation/SideMenu/SideMenu'
 
 class Layout extends Component {
   // state continues info if sideMenu is visible or not
@@ -14,10 +14,16 @@ class Layout extends Component {
     this.setState({ showSideMenu: false })
   }
 
+  sideMenuToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideMenu: !prevState.showSideMenu }
+  })
+}
+
   render () {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar sideMenuToggleClicked={ this.sideMenuToggleHandler}/>
         <SideMenu
           closed={this.sideMenuClosedHandler}
           open={this.state.showSideMenu}
